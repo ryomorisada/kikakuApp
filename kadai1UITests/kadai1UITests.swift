877d7efd35db326590fd615543eced63eaab1d45
森定遼 <seventh.mark.1227.zng@gmail.com>
2016-11-04 06:11:15 -0700

    Initial Commit

---
+++ kadai1UITests/kadai1UITests.swift
@@ -0,0 +1,36 @@
+//
+//  kadai1UITests.swift
+//  kadai1UITests
+//
+//  Created by 森定遼 on 2016/11/04.
+//  Copyright © 2016年 森定遼. All rights reserved.
+//
+
+import XCTest
+
+class kadai1UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
