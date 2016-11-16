f221d7d9b856a24fe16ac04864a0f08dc4d311c2
森定遼 <seventh.mark.1227.zng@gmail.com>
2016-11-15 19:49:48 -0800

    JSON内容変更 テーブルビューセルから質問内容変更

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func BtnCheck(_ sender: AnyObject) {
+        
+    }
+    @IBAction func BtnPhoto(_ sender: AnyObject) {
+    }
+    @IBAction func BtnAlbum(_ sender: AnyObject) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    }
 
---
+++ kadai1/secondViewController.swift
@@ -0,0 +1,58 @@
+//
+//  secondViewController.swift
+//  kadai1
+//
+//  Created by 森定遼 on 2016/11/14.
+//  Copyright © 2016年 森定遼. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    let myGanre = ["服,靴","本","電子機器","ぬいぐるみ","文房具","その他"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //行数の指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myGanre.count
+    }
+    
+    //表示するセルの中身
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myGanre[indexPath.row])"
+        return cell
+    }
+    
+    
+    
+    //選択されたときに行う処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myGanre[indexPath.row])")
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
