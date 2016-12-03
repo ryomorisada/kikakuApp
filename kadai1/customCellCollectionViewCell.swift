702a5d1c09bb3420f672f52a2e7150a14d040188
森定遼 <seventh.mark.1227.zng@gmail.com>
2016-12-03 07:53:28 -0800

    userDefaultデータ受け渡し完了

---
+++ kadai1/customCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  customCellCollectionViewCell.swift
+//  kadai1
+//
+//  Created by 森定遼 on 2016/12/03.
+//  Copyright © 2016年 森定遼. All rights reserved.
+//
+
+import UIKit
+
+class customCellCollectionViewCell: UICollectionViewCell {
+    
+}
